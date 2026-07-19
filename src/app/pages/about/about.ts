2de131fee20932c597a6551259dec553dbe1d418
kilicemre <kilicemre004@gmail.com>
2026-07-19 14:37:04 +0200

    coloring

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Skill {
+  name: string;
+  level: number;
+}
+
 @Component({
   selector: 'app-about',
   standalone: true,
@@ -7,16 +12,46 @@ import { Component } from '@angular/core';
   styleUrl: './about.css',
 })
 export class About {
-  protected readonly skills = [
-    'Angular',
-    'TypeScript',
-    'JavaScript',
-    'Java',
-    'Spring Boot',
-    'Python',
-    'PostgreSQL',
-    'Git',
-    'HTML',
-    'CSS',
+  protected readonly skills: Skill[] = [
+    {
+      name: 'Angular',
+      level: 80,
+    },
+    {
+      name: 'TypeScript',
+      level: 75,
+    },
+    {
+      name: 'JavaScript',
+      level: 70,
+    },
+    {
+      name: 'Java',
+      level: 82,
+    },
+    {
+      name: 'Spring Boot',
+      level: 75,
+    },
+    {
+      name: 'Python',
+      level: 65,
+    },
+    {
+      name: 'PostgreSQL',
+      level: 70,
+    },
+    {
+      name: 'Git',
+      level: 72,
+    },
+    {
+      name: 'HTML',
+      level: 88,
+    },
+    {
+      name: 'CSS',
+      level: 80,
+    },
   ];
 }
